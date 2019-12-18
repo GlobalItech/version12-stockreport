56424e873cd6246f53fda8cfc13eec7d2c19b2ed
ubuntu <ubuntu@iTechServer.Home>
2019-12-18 13:01:46 +0500

    Report1

---
+++ stock_xls_report/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Export Stock Excel Report',
+    'version': '12.0.1.0.0',
+    'category': 'Warehouse',
+    'license': "AGPL-3",
+    'summary': "Current Stock Report for all Products in each Warehouse",
+    'author': 'ItechResources',
+    'company': 'ItechResources',
+    'website': 'http://www.cybrosys.com',
+    'depends': [
+                'base',
+                'stock',
+                'sale',
+                'purchase',
+                'report_xlsx'
+                ],
+    'data': [
+            'views/wizard_view.xml',
+            'views/wh_location.xml',
+            ],
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'price':'10.0',
+    'currency': 'EUR',
+    'auto_install': False,
+}
---
+++ stock_xls_report/report/__init__.py
@@ -0,0 +1,3 @@
+#from . import current_stock_xls
+from . import finished_goods
+
